e391d07af6034e505a0631b81fd40dd74e2b3fa3
Tuhin <towhid.tuhin@selise.ch>
2019-08-03 11:22:24 +0600

    Calculate Factorial synchronously.

--- AsynchronousProgrammingPractice/AsynchronousProgrammingPractice/Program.cs
+++ AsynchronousProgrammingPractice/AsynchronousProgrammingPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace AsynchronousProgrammingPractice
 {
@@ -6,7 +7,33 @@ namespace AsynchronousProgrammingPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int counter = 1; counter < 9; counter++)
+            {
+                if (counter % 3 == 0)
+                {
+                    WriteFactorial(counter);
+                }
+                else
+                {
+                    Console.WriteLine(counter);
+                }
+            }
+            Console.ReadLine();
+        }
+        private static void WriteFactorial(int no)
+        {
+            int result = FindFactorialWithSimulatedDelay(no);
+            Console.WriteLine($"Factorial of {no} is {result}");
+        }
+        private static int FindFactorialWithSimulatedDelay(int no)
+        {
+            int result = 1;
+            for (int i = 1; i <= no; i++)
+            {
+                Thread.Sleep(500);
+                result = result * i;
+            }
+            return result;
         }
     }
 }
