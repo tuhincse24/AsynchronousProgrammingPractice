d6d0d3683c7ada18de575526a09254faf5ef182a
Tuhin <towhid.tuhin@selise.ch>
2019-08-03 11:15:38 +0600

    Add new solution to git repository.

---
+++ AsynchronousProgrammingPractice/AsynchronousProgrammingPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsynchronousProgrammingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
